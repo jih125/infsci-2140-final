6708842b618270b2ae1eaafaa9eb4f99886452cb
Jinpeng Hu <jih125@pitt.edu>
2019-12-05 01:46:35 -0500

    "Card" function
    Fake card added, followed by defined side height

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -3,8 +3,10 @@
     <el-header>
       <el-row>
         <el-col :span="1">
-          <el-link :underline="false" onclick="history.back()">
-            <i class="material-icons md-48 back" style="margin-top:18px;">arrow_back</i>
+          <el-link :underline="false" href="/">
+            <i class="material-icons md-48 back" style="margin-top:18px;"
+              >arrow_back</i
+            >
           </el-link>
         </el-col>
         <el-col :span="20">
@@ -16,8 +18,13 @@
             type="primary"
             size="mini"
             style="margin-top: 18px;"
-          >Outline</el-button>
-          <el-drawer title="Outline" :visible.sync="drawer" :direction="direction">
+            >Outline</el-button
+          >
+          <el-drawer
+            title="Outline"
+            :visible.sync="drawer"
+            :direction="direction"
+          >
             <el-menu default-active="2" class="el-menu-vertical-demo">
               <el-submenu index="1">
                 <template slot="title">
@@ -45,92 +52,113 @@
     <el-container>
       <el-container>
         <el-main>
-          <iframe src="demo.pdf" width="100%" height="800px" style="border: none;" />
+          <iframe
+            src="demo.pdf"
+            width="100%"
+            height="800px"
+            style="border: none;"
+          />
         </el-main>
         <el-footer>INFSCI 2140 PROJECT</el-footer>
       </el-container>
-      <el-aside width="25%" style="overflow:auto;">
+      <el-aside width="25%" style="height:800px;" id="sidearea">
         <h5>Search methods</h5>
         <el-form ref="form" :model="form" name="testform">
           <el-form-item prop="searchtext">
-            <el-input type="textarea" v-model="form.searchtext" :rows="3" id="sel"></el-input>
+            <el-input
+              type="textarea"
+              v-model="form.searchtext"
+              :rows="3"
+              id="sel"
+            ></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="submit('form')" :loading="loading">Submit</el-button>
+            <el-button type="primary" @click="submit('form')" :loading="loading"
+              >Submit</el-button
+            >
             <el-button @click="reset('form')">Cancel</el-button>
           </el-form-item>
         </el-form>
-        <div v-show="show" id="result">
-          <el-card class="google-scholar">
-            <div slot="header">
-              <h1>Search Result:</h1>
-              <span id="result-title"></span>
-            </div>
-            <div>
-              <span id="result-content"></span>
-            </div>
-          </el-card>
-        </div>
       </el-aside>
     </el-container>
   </el-container>
 </template>
 <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
 <script>
+import Vue from 'vue'
+import card from './card.vue'
+Vue.config.productionTip = false
+
 export default {
+  components: {
+    card
+  },
   data() {
     return {
       show: true,
       results: [],
       loading: false,
       drawer: false,
-      direction: "ltr",
+      direction: 'ltr',
       paper: {
-        title: "A study of methods for negative relevance feedback",
-        src: "demo.pdf"
+        title: 'A study of methods for negative relevance feedback',
+        src: 'demo.pdf'
       },
       form: {
-        searchtext: ""
+        searchtext: ''
       },
       result: {
-        text: ""
+        text: ''
       }
-    };
+    }
   },
   computed: {},
   methods: {
     submit(formName) {
-      this.loading = true;
-      if (this.form.searchtext == "") {
+      this.loading = true
+      if (this.form.searchtext == '') {
         setTimeout(
-          () => ((this.loading = false), alert("Please submit your search!")),
+          () => ((this.loading = false), alert('Please submit your search!')),
           1000
-        );
+        )
       } else {
-        setTimeout(
-          () => (
-            (this.loading = false),
-            (document.getElementById(
-              "result-title"
-            ).innerText = "'"+this.form.searchtext + "'"),
-            (document.getElementById(
-              "result-content"
-            ).innerText = this.result.text)
-          ),
-          1000
-        );
+        setTimeout(() => ((this.loading = false), this.createCard()), 1000)
       }
     },
     reset(formName) {
-      this.$refs[formName].resetFields();
+      this.$refs[formName].resetFields()
     },
-    createCard() {
-      
+    createCard() { 
+      var title = document.createElement('h1')
+      var titleText = document.createTextNode('Search result from Wikipedia:')
+      title.appendChild(titleText)
+      document.getElementById('sidearea').appendChild(title)
+
+      var content = document.createElement('p')
+      var contentText = document.createTextNode(this.form.searchtext)
+      content.appendChild(contentText)
+      document.getElementById('sidearea').appendChild(contentText)
+
+      var referenceTitle = document.createElement('h1')
+      referenceTitle.appendChild(document.createTextNode('Reference:'))
+      document.getElementById('sidearea').appendChild(referenceTitle)
+
+      var divider = document.createElement('hr')
+      var referenceArray = ['Bullying', 'Cyber', 'Wiki'];
+      for (var i = 0; i < referenceArray.length; i++) {
+        var reference = document.createElement('a');
+        var referencetext = document.createTextNode(referenceArray[i]);
+        reference.appendChild(referencetext);
+        reference.href = 'http://www.google.com';
+        reference.title = referenceArray[i];
+        reference.style.marginRight = '10px'
+        document.getElementById('sidearea').appendChild(reference);
+        document.getElementById('sidearea').appendChild(divider);
+      }
     }
   }
-};
+}
 </script>
-
 <style scoped>
 .el-header {
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
---
+++ src/components/card.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-card :class="type">
+    <div slot="header">
+      <h1>Search result</h1>
+      <p>{{ title }}</p>
+    </div>
+    <div id="content">
+      <p>{{ description }}</p>
+      <a :href="link"></a>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: ['title','description','link'],
+  data() {
+    return {
+      cards: [
+        {
+          card: { title: '', description: '', link: ''},
+          cardtype: 'card'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
