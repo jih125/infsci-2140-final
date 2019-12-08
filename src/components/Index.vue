ade72764788e5dfbf9b6e9b1d5f44601ab51309a
Sylvia Tian <sylviatian935@gmail.com>
2019-12-08 16:59:04 -0500

    Update Index.vue

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -83,20 +83,20 @@ export default {
            ['case study in the design of Vitro, a voice-based assistant created for a biology wet lab. ', 'contextual inquiry proces'],
           findings: 
           ['Participants saw Vitro as lab equipment, rather than as a labmate. ',
-          'Despite our initial observation that scientists engaged in “care- ful deviation” from protocols, Vitro supported this practice only minimally. ',
-          'On average, participants in our experiment were only “mod- erately comfortable” with how voice-enabled devices such as Google Assistant, Siri or Cortana handle user privacy.' ],
-          contribution: 'It does not seek to report on a new AI technology, nor does it purport to introduce a new design technique or methodology. Instead, it simply asks if (and how) human-computer interaction and design practition- ers and researchers may take voice as a serious design material for hands-free workplace applications',
+          'Despite our initial observation that scientists engaged in “careful deviation” from protocols, Vitro supported this practice only minimally. ',
+          'On average, participants in our experiment were only “moderately comfortable” with how voice-enabled devices such as Google Assistant, Siri or Cortana handle user privacy.' ],
+          contribution: 'It does not seek to report on a new AI technology, nor does it purport to introduce a new design technique or methodology. Instead, it simply asks if (and how) human-computer interaction and design practitioners and researchers may take voice as a serious design material for hands-free workplace applications',
           url: '/p1531-cambre(1)'
         },
         {
           title:
-            'Context-Sensitive Information Retrieval Using Implicit Feedback',
-          keyword: 'Negative feedback',
+            'Measuring Young Children’s Long-Term Relationships with Social Robots',
+          keyword: 'assessments; children; long-term interaction; human-robot interaction; social robots',
           methodology:
-            'Given a query and a ranked document list for the query, we keep deleting the top ranked relevant document until none of the top 10 ranked documents of the list is relevant. We assume that the deleted relevant documents do not exist in the collection.',
-          findings: 'here',
-          contribution: 'here',
-          url: '/hofmann'
+            'Experiment: social vs. neutral\n A pilot test of four assessments that we have adapted or created for use in this context with children aged 5–6: the Inclusion of Other in Self task, the Social-Relational Interview, the Narrative Description, and the Self-disclosure Task.',
+          findings: 'Children can appropriately respond to these assessments with reasonably high internal reliability, and that the proposed assessments are able to capture child-robot relationship adjustments over a long-term interaction.',
+          contribution: 'This paper makes two contributions. First, we provide reliability and validity information about the assessments. Second, we report the results of the field study. ',
+          url: '/p207-kory_westlund'
         }
       ]
     }
